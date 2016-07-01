546295cf31d53eac1e83b82771b847c455b77792
devin <taxing66@163.com>
2016-07-01 23:48:22 +0800

    code the ResingBottomDialog

--- app/src/main/java/com/joiway/devin/holiday/activity/MainActivity.java
+++ app/src/main/java/com/joiway/devin/holiday/activity/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Toast;
 import com.joiway.devin.holiday.BuildConfig;
 import com.joiway.devin.holiday.R;
 import com.joiway.devin.holiday.tools.LogManager;
+import com.joiway.devin.holiday.view.ResingBottomDialog;
 
 public class MainActivity extends AbsActivity {
     private TextView tvTitle;
@@ -20,5 +21,7 @@ public class MainActivity extends AbsActivity {
          tvTitle = (TextView) findViewById(R.id.tv_title);
          tvTitle.setText(R.string.happy_everyday);
           LogManager.logDebug(LogManager.DEVELOPER_DEVIN,"MainActivity","onCreate","test_debug");
+        ResingBottomDialog dialog = ResingBottomDialog.getInstance(this);
+        dialog.show();
     }
 }
